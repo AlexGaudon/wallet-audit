891d2971d90a9fb9c028e0680ebb161a76d8b2b6
alexgaudon <agaudon@proton.me>
2024-02-08 21:14:59 -0330

    initial commit

---
+++ app/signin/page.tsx
@@ -0,0 +1,9 @@
+import { SignInForm } from "@/components/sign-in-form";
+
+export default async function SignInPage() {
+  return (
+    <main className="mt-4">
+      <SignInForm />;
+    </main>
+  );
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,9 @@
+import { SignUpForm } from "@/components/sign-up-form";
+
+export default async function SignUpPage() {
+  return (
+    <main className="mt-4">
+      <SignUpForm />;
+    </main>
+  );
+}
