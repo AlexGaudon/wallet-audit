76e1403070101da0f5c9d53c1a0b0e25f0967bf1
alexgaudon <agaudon@proton.me>
2024-02-22 18:35:25 -0330

    only show 5 top transactions

--- lib/actions.ts
+++ lib/actions.ts
@@ -104,15 +104,7 @@ export async function getTopSpendingThisMonth() {
       x.expand?.category?.name !== "Investments" &&
       x.expand?.category?.name !== "Transfer"
   );
-  const categorizedThisMonth = await getCategorizedSpendingForPeriod(
-    new Date().toString()
-  );
-
-  const keys = Array.from(categorizedThisMonth.keys());
-
-  const length = Math.max(10, keys.length);
-
-  return filtered.slice(0, length);
+  return filtered.slice(0, 5);
 }
 
 export async function deleteTransaction(transactionId: string) {
